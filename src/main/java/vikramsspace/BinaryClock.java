101716157bb2b0377900bfda449992f636f0b959
vikravish <134341020+vikravish@users.noreply.github.com>
2024-08-12 17:29:05 -0400

    start getAllCombos funct

--- src/main/java/vikramsspace/BinaryClock.java
+++ src/main/java/vikramsspace/BinaryClock.java
@@ -15,6 +15,7 @@ public class BinaryClock {
         
         //n = 3, subsequenceLength = 8
         double subsequenceLength = Math.pow(2, n);
+        int subsequenceLengthInt = (int) Math.round(subsequenceLength);
         //000, 001, 010, 101, 011, 111, 110, 100 - First 8 three-digit subsequences
         //  0,   1,   2,   5,   3,   7,   6,   4
         ArrayList<String> subsequences = new ArrayList<>();
@@ -25,6 +26,23 @@ public class BinaryClock {
 
         ArrayList<ArrayList<String>> allSubsequences = new ArrayList<>();
 
+        for (int i = 0; i > subsequenceLengthInt; i++){
+            int[] currentSequence = new int[subsequenceLengthInt];
+            for (int j = 0; j < subsequenceLengthInt; j++){
+                
+            }
+        }
         //TODO: Make all combinations and only take the ones of which all subsequences are dinstinct.
     }
+    public static void getAllCombos(ArrayList<Integer> intList, int index){
+        for (int i = index; i > intList.size(); i++){
+            if (index < intList.size()){
+            getAllCombos(intList, i+1);
+            }
+            if(i > intList.size()-1){
+                return;
+            }
+        }
+    }
+
 }
